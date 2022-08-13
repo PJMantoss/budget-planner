fa62c95b2389e3c9a0fdc1dce280044999ac4855
PJMantoss <joelptoss@gmail.com>
2022-08-13 17:26:29 +0100

    update scripts

--- script/BudgetPlanner.js
+++ script/BudgetPlanner.js
@@ -2,5 +2,8 @@ export default class BudgetPlanner {
     constructor(querySelectorString){}
 
     //method that returns html for the actual table
-    static html()
+    static html(){}
+
+    //returns html string for a row inside the html table
+    static entryHtml(){}
 }

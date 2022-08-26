88ba1d56facd1f3af0e956c2cfaa08a3334f3688
PJMantoss <joelptoss@gmail.com>
2022-08-26 20:51:15 +0100

    update BudgetPlanner

--- script/BudgetPlanner.js
+++ script/BudgetPlanner.js
@@ -95,7 +95,9 @@ export default class BudgetPlanner {
     //so that it persists when browser is refreshed
     save(){
         //take all rows & convert to an array of objects to be saved
-        const data = this.getEntryRows().map()
+        const data = this.getEntryRows().map(row => {
+            return {}
+        })
     }
 
     //takes in an object param and adds new entry inside the table.

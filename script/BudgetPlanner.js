6f6506d758c45870a9d890e4c598c3c4993c6a84
PJMantoss <joelptoss@gmail.com>
2022-08-27 20:07:42 +0100

    update BudgetPlanner

--- script/BudgetPlanner.js
+++ script/BudgetPlanner.js
@@ -90,6 +90,7 @@ export default class BudgetPlanner {
     //takes all the current rows in the table, 
     //calculate their total & display result in the bottom right
     updateSummary(){
+        // take every single row and convert to total
         const total = this.getEntryRows()
     }
 
